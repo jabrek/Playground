e287e2214a47342b8c380177bf57c9373f840293
jabrek <jarek.dulak@gmail.com>
2015-11-05 21:45:51 +0100

    initial version of message processing with spring boot from Spring Guide

---
+++ MessageRouter/src/main/java/org/lor/messagerouter/sample/Application.java
@@ -0,0 +1,54 @@
+package org.lor.messagerouter.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.util.FileSystemUtils;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import java.io.File;
+
+/**
+ * Created by jarek on 05.11.15.
+ */
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+
+    @Bean
+        // Strictly speaking this bean is not necessary as boot creates a default
+    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // Clean out any ActiveMQ data from a previous run
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+
+        // Launch the application
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // Send a message
+        MessageCreator messageCreator = new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                return session.createTextMessage("ping!");
+            }
+        };
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        System.out.println("Sending a new message.");
+        jmsTemplate.send("mailbox-destination", messageCreator);
+    }
+}
